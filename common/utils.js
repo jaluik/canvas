83ecdf475677e6e1ba126ff2079eb7242cc97c11
jaluik <cqzhengmouren@gmail.com>
2020-05-05 21:35:35 +0800

    增加canvas api demo

--- common/utils.js
+++ common/utils.js
@@ -30,3 +30,34 @@ utils.captureMouse = function (element) {
 
   return mouse;
 };
+
+utils.colorToRGB = function (color, alpha = 1) {
+  if (typeof color === "string" && color[0] === "#") {
+    color = window.parseInt(color.slice(1), 16);
+  }
+  var r = (color >> 16) & 0xff;
+  var g = (color >> 8) & 0xff;
+  var b = color & 0xff;
+  alpha = alpha < 0 || alpha > 1 ? 0 : alpha;
+  if (alpha === 1) {
+    return `rgb(${r},${g},${b})`;
+  }
+  return `rgba(${r},${g},${b}, ${alpha})`;
+};
+
+utils.parseColor = function (color, toNumber) {
+  if (toNumber === true) {
+    if (typeof color === "number") {
+      return color | 0;
+    }
+    if (typeof color === "string" && color[0] === "#") {
+      color = color.slice(1);
+    }
+    return window.parseInt(color, 16);
+  } else {
+    if (typeof color === "number") {
+      color = `#${("00000" + (color | 0).toString(16)).substr(-6)}`;
+    }
+    return color;
+  }
+};
