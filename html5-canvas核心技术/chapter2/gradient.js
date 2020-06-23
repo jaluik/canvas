64fd4694e6d3ddc162f0ab4c843a622740642a8f
jaluik <cqzhengmouren@gmail.com>
2020-06-23 22:25:21 +0800

    放射性渐变

---
+++ html5-canvas核心技术/chapter2/gradient.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("canvas"),
+  context = canvas.getContext("2d"),
+  gradient = context.createLinearGradient(0, 0, canvas.width, canvas.height);
+
+gradient.addColorStop(0, "blue");
+gradient.addColorStop(0.25, "white");
+gradient.addColorStop(0.5, "purple");
+gradient.addColorStop(0.75, "red");
+gradient.addColorStop(1, "yellow");
+
+context.fillStyle = gradient;
+context.fillRect(0, 0, canvas.width, canvas.height);
